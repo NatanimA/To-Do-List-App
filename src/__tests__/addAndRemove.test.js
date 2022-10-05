710f053a0f487ff4e6dc3959bbc13ac27535f8ac
Ahmed Nazir Musah <ahmed999nazir@gmail.com>
2022-10-05 16:42:16 +0000

    fix Eslint and Stylelint errors

--- src/__tests__/addAndRemove.test.js
+++ src/__tests__/addAndRemove.test.js
@@ -2,27 +2,23 @@
  * @jest-environment jsdom
  */
 import TasksObject from '../modules/tasksObject.js';
-import LocalStorage from '../modules/localStorage.js'
+import LocalStorage from '../modules/localStorage.js';
 
 describe('Addition and removal of the tasks', () => {
-    test('Check add Tasks', () => {
-        const tasks = new TasksObject('Celebrate',false,0);
-        LocalStorage.addToLocalDataStorage(tasks);
-        const todo = LocalStorage.getFromLocalStorage();
-        expect(todo).toEqual([{
-            description: 'Celebrate',
-            completed: false,
-            index: 0,
-        }]);
-    });
+  test('Check add Tasks', () => {
+    const tasks = new TasksObject('Celebrate', false, 0);
+    LocalStorage.addToLocalDataStorage(tasks);
+    const todo = LocalStorage.getFromLocalStorage();
+    expect(todo).toEqual([{
+      description: 'Celebrate',
+      completed: false,
+      index: 0,
+    }]);
+  });
 
-    test('Check Removal of tasks', () => {
-        LocalStorage.removeFromLocalDataStorage('Celebrate');
-        const toDos = LocalStorage.getFromLocalStorage();
-        expect(toDos).toEqual([]);
-    });
+  test('Check Removal of tasks', () => {
+    LocalStorage.removeFromLocalDataStorage('Celebrate');
+    const toDos = LocalStorage.getFromLocalStorage();
+    expect(toDos).toEqual([]);
+  });
 });
-
-
-
-
--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -1,11 +1,9 @@
 export default class LocalDataStorage {
-  
-  static getFromLocalStorage(){
+  static getFromLocalStorage() {
     let tasks;
-    if(localStorage.getItem('test') === null){
-      tasks = []
-    }
-    else{
+    if (localStorage.getItem('test') === null) {
+      tasks = [];
+    } else {
       tasks = JSON.parse(localStorage.getItem('test'));
     }
     return tasks;
@@ -14,19 +12,18 @@ export default class LocalDataStorage {
   static addToLocalDataStorage(tasks) {
     const task = LocalDataStorage.getFromLocalStorage();
     task.push(tasks);
-    localStorage.setItem('test',JSON.stringify(task));
-    localStorage.setItem('todo',JSON.stringify(tasks));
+    localStorage.setItem('test', JSON.stringify(task));
+    localStorage.setItem('todo', JSON.stringify(tasks));
   }
 
   static removeFromLocalDataStorage(desc) {
     const tasks = LocalDataStorage.getFromLocalStorage();
     const filter = tasks.filter((task) => task.description !== desc);
 
-    for(let index = 0 ; index <filter.length ; index ++){
+    for (let index = 0; index < filter.length; index += 1) {
       filter[index].index = index;
     }
-    
+
     localStorage.setItem('test', JSON.stringify(filter));
   }
 }
-
